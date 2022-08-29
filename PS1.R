20ecd0fa993c5e9fb6f46090d2b5bb2a1857c5b4
scaraballoc <112407182+scaraballoc@users.noreply.github.com>
2022-08-29 16:27:10 -0500

    Update PS1.R

--- PS1.R
+++ PS1.R
@@ -1,34 +1,22 @@
-#==============================================================================#
-# Problem Set #1, BD&ML                                                        #
-# David Santiago Caraballo, 
-# Sergio David Pinilla, 
-# Juan Diego Valencia, 
-#==============================================================================#
+#=============================================================================#
+#      Problem Set 1 Big Data and Machine Learning for Applied Economics      #
+#           David Santiago Caraballo Candela             201813007            #
+#           Sergio David Pinilla Padilla                 201814755            #
+#           Juan Diego Valencia Romero                   201815561            #
+#=============================================================================#
 
-
-# Initial configuration
-
+#Initial workspace configuration
 rm(list=ls()) 
-sys.setlocale("LC_CTYPE", "en_US.UTF-8")
-
+sys.setlocale("LC_CTYPE","en_US.UTF-8")
 require(pacman)
 p_load(tidyverse,data.table,plyr,rvest,XML,xml2)
 
-
-# Read pages
-
-dc_url <- paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 
-                   1:10, ".html")
-
-df <- data.frame()
-for (url in dc_url) {
+#Read pages
+dc_url<-paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 1:10, ".html")
+df<-data.frame()
+for (url in dc_url){
   print(url)
-  temp <- read_html(url) %>% 
-    html_table() 
-  temp <- as.data.frame(temp[[1]])
-  df <- rbind(df, temp)
+  temp<-read_html(url) %>% html_table() 
+  temp<-as.data.frame(temp[[1]])
+  df<-rbind(df, temp)
 }
-
-
-
-
